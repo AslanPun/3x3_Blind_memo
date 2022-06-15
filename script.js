6a75cbfdbcbc18d4bbbf559cee8432f6ea66f216
Basicosmic <85536740+Basicosmic@users.noreply.github.com>
2022-06-14 21:59:10 -0700

    Update script.js

--- script.js
+++ script.js
@@ -18,9 +18,23 @@ function isEven(str){
     return length % 2 == 0
 }
 
+function addSpaceEvery2letter(str){
+    if(isEven(str)){
+        spacedStr = str.replace(/.{1,2}(?=(.{2})+$)/g, '$& '); // Add spaces between every 2 letter
+        return spacedStr
+    }
+    else{
+        str = str + "a"
+        spacedStr = str.replace(/.{1,2}(?=(.{2})+$)/g, '$& '); // Add spaces between every 2 letter
+        spacedStr = spacedStr.slice(0, spacedStr.length - 1)
+        return spacedStr
+    }
+
+    
+}
 
 function linkingV2(element) {
-    var currentColor = window.getComputedStyle( element ,null).getPropertyValue('background-color');
+    // var currentColor = window.getComputedStyle( element ,null).getPropertyValue('background-color');
     var id = element.id;
     var needChange = document.querySelectorAll("#"+id);
     let html = element.innerHTML;
@@ -33,21 +47,28 @@ function linkingV2(element) {
         let edge = document.getElementById("edge").innerHTML;
         edge = edge + html
         edge = edge.replace(/\s+/g, ''); //remove all spaces in the str
+        spacedEdge = addSpaceEvery2letter(edge)
+        
+        document.getElementById("edge").innerHTML = spacedEdge;
+        // if(isEven(edge)){
 
-        if(isEven(edge)){
-            spacedEdge = edge.replace(/.{1,2}(?=(.{2})+$)/g, '$& '); // Add spaces between every 2 letter
+        //     spacedEdge = add2spaces(edge)
+        //     // spacedEdge = edge.replace(/.{1,2}(?=(.{2})+$)/g, '$& '); // Add spaces between every 2 letter
 
     
-            document.getElementById("edge").innerHTML = spacedEdge;
-        }
-        else{ // edge is odd numbered letter
-            edge = edge + "a"
-            spacedEdge = edge.replace(/.{1,2}(?=(.{2})+$)/g, '$& '); // Add spaces between every 2 letter
-
-            spacedEdge = spacedEdge.slice(0, spacedEdge.length - 1)
+        //     document.getElementById("edge").innerHTML = spacedEdge;
+        // }
+        // else{ // edge is odd numbered letter
             
-            document.getElementById("edge").innerHTML = spacedEdge;
-        }
+        //     spacedEdge = add2spaces(edge)
+
+        //     // edge = edge + "a"
+        //     // spacedEdge = edge.replace(/.{1,2}(?=(.{2})+$)/g, '$& '); // Add spaces between every 2 letter
+
+        //     // spacedEdge = spacedEdge.slice(0, spacedEdge.length - 1)
+            
+        //     document.getElementById("edge").innerHTML = spacedEdge;
+        // }
         
 
 
@@ -58,21 +79,24 @@ function linkingV2(element) {
         let corner = document.getElementById("corner").innerHTML;
         corner = corner + html
         corner = corner.replace(/\s+/g, ''); //remove all spaces in the str
+        spacedCorner = addSpaceEvery2letter(corner)
+        
+        document.getElementById("corner").innerHTML = spacedCorner;
 
-        if(isEven(corner)){
-            spacedCorner = corner.replace(/.{1,2}(?=(.{2})+$)/g, '$& '); // Add spaces between every 2 letter
+        // if(isEven(corner)){
+        //     spacedCorner = corner.replace(/.{1,2}(?=(.{2})+$)/g, '$& '); // Add spaces between every 2 letter
 
     
-            document.getElementById("corner").innerHTML = spacedCorner;
-        }
-        else{ // corner is odd numbered letter
-            corner = corner + "a"
-            spacedCorner = corner.replace(/.{1,2}(?=(.{2})+$)/g, '$& '); // Add spaces between every 2 letter
+        //     document.getElementById("corner").innerHTML = spacedCorner;
+        // }
+        // else{ // corner is odd numbered letter
+        //     corner = corner + "a"
+        //     spacedCorner = corner.replace(/.{1,2}(?=(.{2})+$)/g, '$& '); // Add spaces between every 2 letter
 
-            spacedCorner = spacedCorner.slice(0, spacedCorner.length - 1)
+        //     spacedCorner = spacedCorner.slice(0, spacedCorner.length - 1)
             
-            document.getElementById("corner").innerHTML = spacedCorner;
-        }
+        //     document.getElementById("corner").innerHTML = spacedCorner;
+        // }
 
     }
     
@@ -191,3 +215,20 @@ function changeMode() {
     reset()
 }
 
+function undoEdge(){
+    let edge = document.getElementById("edge").innerHTML;
+    edge = edge.replace(/\s+/g, ''); //remove all spaces
+    oneLessEdge = edge.slice(0, edge.length - 1) //take away one edge
+    oneLessEdge = addSpaceEvery2letter(oneLessEdge) // add space every 2 letter
+    
+    document.getElementById("edge").innerHTML = oneLessEdge;
+}
+
+function undoCorner(){
+    let corner = document.getElementById("corner").innerHTML;
+    corner = corner.replace(/\s+/g, ''); //remove all spaces
+    oneLessCorner = corner.slice(0, corner.length - 1) //take away one corner
+    oneLessCorner = addSpaceEvery2letter(oneLessCorner)
+
+    document.getElementById("corner").innerHTML = oneLessCorner;
+}
